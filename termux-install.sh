44682d222c0b040a8c941ee884c869cd1da38ad3
kevin010717 <k511153362@gmail.com>
2024-09-14 10:44:04 +0800

    1

--- termux-install.sh
+++ termux-install.sh
@@ -20,6 +20,7 @@ BLUE_COLOR='\e[1;34m'
 PINK_COLOR='\e[1;35m'
 SHAN='\e[1;33;5m'
 RES='\e[0m'
+
 get-local-ipv4-using-hostname() {
   hostname -I 2>&- | awk '{print $1}'
 }
@@ -51,7 +52,44 @@ get-local-ipv4-select() {
     grep -m 1 "^10\." <<<"$ips" ||
     head -n 1 <<<"$ips"
 }
-
+termux-api() {
+  termux-battery-status
+  termux-camera-info
+  termux-contact-list
+  termux-infrared-frequencies
+  termux-telephony-cellinfo
+  termux-telephony-deviceinfo
+  termux-tts-engines
+  termux-wifi-connectioninfo
+  termux-wifi-scaninfo
+  termux-brightness auto
+  termux-camera-photo -c 0 termux-camera-photo.jpg
+  rm -rf termux-camera-photo.jpg
+  termux-clipboard-set PHP是世界上最好的语言
+  termux-clipboard-get
+  termux-infrared-transmit -f 20,50,20,30
+  termux-location -p network
+  termux-microphone-record -d -f 1.m4a
+  termux-microphone-record -q
+  rm 1.m4a
+  termux-notification -t '国光的Termu通知测试' -c 'Hello Termux' --type default
+  termux-toast -b white -c black Hello Termux
+  termux-torch on
+  termux-torch off
+  termux-tts-speak 'Hello world!'
+  termux-vibrate -f -d 3000
+  termux-wallpaper -l -f
+  termux-wifi-enable false
+  termux-wifi-enable true
+  termux-dialog confirm -i 'Hello Termux' -t 'confirm测试'
+  termux-dialog checkbox -v 'Overwatch,GTA5,LOL' -t '平时喜欢玩啥游戏'
+  termux-dialog date -d 'yyyy-MM-dd' -t '你的出生日期是?'
+  termux-dialog radio -v '小哥哥,小姐姐' -t '你的性别是?'
+  termux-dialog sheet -v '菜鸡,国光'
+  termux-dialog spinner -v '国光,光光' -t '你最喜欢的博主是?'
+  termux-dialog text -i '密码:' -t '请输入核弹爆炸密码'
+  termux-dialog time -t '你每天多少点睡觉?'
+}
 install-update() {
   echo "bell-character = ignore" >>~/.termux/termux.properties
   termux-reload-settings
@@ -199,9 +237,9 @@ install-mpv-termux-url-opener() {
   #配置termux-url-opener
   mkdir -p ~/bin
   echo 'echo "1.download it" 
-echo "2.listen to it"  
-read choice 
-case $choice in 
+  echo "2.listen to it"  
+  read choice 
+  case $choice in 
   1) yt-dlp --output "%(title)s.%(ext)s" --merge-output-format mp4 --embed-thumbnail --add-metadata -f "bestvideo[height<=1080]+bestaudio[ext=m4a]" $1;; 
   2) mpv --no-video -v $1;;
   *) mpv --no-video -v $1;;
@@ -413,6 +451,94 @@ fun() {
     *) break ;;
     esac
   done
+}
+nvim() {
+  echo -e "${GREEN_COLOR}1.astronvim${RES}"
+  echo -e "${GREEN_COLOR}2.lazyvim${RES}"
+  echo -e "${GREEN_COLOR}3.lunarvim${RES}"
+  echo -e "${GREEN_COLOR}4.nvchad${RES}"
+  read choice
+  case $choice in
+  1)
+    if [ ! -d ~/.config/nvim.lunarvim ]; then
+      mv ~/.config/nvim ~/.config/nvim.lunarvim
+      mv ~/.config/nvim.astronvim ~/.config/nvim
+      echo "lunarvim backuped"
+      nvim
+    elif [ ! -d ~/.config/nvim.lazyvim ]; then
+      mv ~/.config/nvim ~/.config/nvim.lazyvim
+      mv ~/.config/nvim.astronvim ~/.config/nvim
+      echo lazyvim backuped
+      nvim
+    elif [ ! -d ~/.config/nvim.nvchad ]; then
+      mv ~/.config/nvim ~/.config/nvim.nvchad
+      mv ~/.config/nvim.astronvim ~/.config/nvim
+      echo "nvchad backuped"
+      nvim
+    fi
+    ;;
+  2)
+    if [ ! -d "~/.config/nvim.nvchad" ]; then
+      mv ~/.config/nvim ~/.config/nvim.nvchad
+      mv ~/.config/nvim.lazyvim ~/.config/nvim
+      echo "nvchad backuped"
+      nvim
+    elif [ ! -d "~/.config/nvim.lunarvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.lunarvim
+      mv ~/.config/nvim.lazyvim ~/.config/nvim
+      echo "lunarvim backuped"
+      nvim
+    elif [ ! -d "~/.config/nvim.astronvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.astronvim
+      mv ~/.config/nvim.lazyvim ~/.config/nvim
+      echo "astronvim backuped"
+      nvim
+    fi
+    ;;
+  3)
+    if [ ! -d "~/.config/nvim.nvchad" ]; then
+      mv ~/.config/nvim ~/.config/nvim.nvchad
+      mv ~/.config/nvim.lunarvim ~/.config/nvim
+      echo "nvchad backuped"
+      nvim
+    elif [ ! -d "~/.config/nvim.lazyvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.lazyvim
+      mv ~/.config/nvim.lunarvim ~/.config/nvim
+      echo "lazyvim backuped"
+      nvim
+    elif [ ! -d "~/.config/nvim.astronvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.astronvim
+      mv ~/.config/nvim.lunarvim ~/.config/nvim
+      echo "astronvim backuped"
+      nvim
+    fi
+    ;;
+  4)
+    if [ ! -d "~/.config/nvim.lunarvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.lunarvim
+      mv ~/.config/nvim.nvchad ~/.config/nvim
+      echo "lunarvim backuped"
+      nvim
+    elif [ ! -d "~/.config/nvim.lazyvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.lazyvim
+      mv ~/.config/nvim.nvchad ~/.config/nvim
+      echo "lazyvim backuped"
+      nvim
+    elif [ ! -d "~/.config/nvim.astronvim" ]; then
+      mv ~/.config/nvim ~/.config/nvim.astronvim
+      mv ~/.config/nvim.nvchad ~/.config/nvim
+      echo "astronvim backuped"
+      nvim
+    fi
+    ;;
+  *) break ;;
+  esac
+}
+tmux-conf() {
+  cat <<EOF >~/.tmux.conf
+set -g status off
+bind-key -n C-a send-prefix
+EOF
 }
 while true; do
   echo -e "${GREEN_COLOR}1.install${RES}"
