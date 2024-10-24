e5c8596328ca5aa8578a9ea41c7f00a8aae64641
kevin010717 <52340130+kevin010717@users.noreply.github.com>
2024-10-24 16:18:33 +0800

    Update ubuntu.sh

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -143,6 +143,8 @@ EOF
 		echo '%kevin     ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
 		#docker
 		sudo systemctl start docker && sudo systemctl enable docker
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
 		;;
 	esac
 }
