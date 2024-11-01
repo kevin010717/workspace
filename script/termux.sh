f72a26031cd1d0f372bb143407a5de28bbcd40e4
kevin010717 <k511153362@gmail.com>
2024-11-01 17:52:14 +0800

    1

--- script/termux.sh
+++ script/termux.sh
@@ -419,22 +419,17 @@ busybox umount $UBUNTUPATH/sdcard
 "
 }
 
-prootdebian-start(){
-    # 中止所有舊行程
+prootubuntu-start(){
     killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
-    # 啟動Termux X11
     am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
     XDG_RUNTIME_DIR=${TMPDIR}
     termux-x11 :0 -ac &
     sleep 3
-    # 啟動PulseAudio
     pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
     pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
-    # 啟動GPU加速的virglserver
     virgl_test_server_android &
-    # 登入proot Debian並啟動桌面環境
+    proot-distro login ubuntu --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session i3"
     #proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
-    proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session i3"
 }
 
 termuxx11-start(){
@@ -446,8 +441,8 @@ termuxx11-start(){
     pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
     pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
     virgl_test_server_android &
-    #termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session"
     termux-x11 :0 -xstartup "dbus-launch --exit-with-session i3"
+    #termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session"
     #termux-x11 :0 -xstartup "dbus-launch --exit-with-session openbox-session"
 }
 
