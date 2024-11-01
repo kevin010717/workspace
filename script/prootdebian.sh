03edf597e6166b636c38a1f5f9ab0f28b935cb04
kevin010717 <k511153362@gmail.com>
2024-11-01 16:25:40 +0800

    1

--- script/prootdebian.sh
+++ script/prootdebian.sh
@@ -5,6 +5,7 @@ apt install git sudo vim firefox-esr i3 dbus-x11 konsole kitty gnome-terminal -y
 apt install i3 rofi picom feh kitty alacritty polybar pavucontrol flameshot
 update-alternatives --config x-terminal-emulator
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ~/.config/polybar-themes && chmod +x ~/.config/polybar-themes/setup.sh && ~/.config/polybar-themes/setup.sh
+git clone https://github.com/kevin010717/workspace.git ~/.workspace
 passwd
 groupadd storage
 groupadd wheel
@@ -13,5 +14,3 @@ useradd -m -g users -G wheel,audio,video,storage -s /bin/bash user
 passwd user
 echo "user ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
 su user
-git clone https://github.com/kevin010717/workspace.git ~/.workspace
-update-alternatives --config x-terminal-emulator
--- script/prootubuntu.sh
+++ script/prootubuntu.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+#tsinghua sources
+apt update && apt install ca-certificates -y
 apt autopurge snapd
-apt update
-apt install sudo vim software-properties-common
-sudo add-apt-repository ppa:mozillateam/ppa && sudo apt update && sudo apt install firefox-esr
-sudo sed -i 's/ports.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+touch /etc/apt/sources.list && cat <<EOF >> /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble main universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-updates main universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-security main universe multiverse
+EOF
+apt install software-properties-common -y && add-apt-repository ppa:mozillateam/ppa && apt update && apt install firefox-esr
+apt install zsh -y && sh -c "$(curl -fsSL https://install.ohmyz.sh/)" && chsh -s $(which zsh)
+wget -O .yazi.zip https://github.com/sxyazi/yazi/releases/download/nightly/yazi-aarch64-unknown-linux-gnu.zip && unzip .yazi.zip -d /usr/bin/ && cp /usr/bin/yazi-aarch64-unknown-linux-gnu/yazi /usr/bin/yazi
+
+passwd
+groupadd storage
+groupadd wheel
+groupadd video
+useradd -m -g users -G wheel,audio,video,storage -s /bin/bash user
+passwd user
+echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
+su user
+cd
+
+#apt install xubuntu-desktop
+#apt install kubuntu-desktop
+#apt install ubuntu-desktop
+apt install git sudo vim i3 dbus-x11 konsole kitty gnome-terminal -y
+apt install i3 rofi picom feh kitty alacritty polybar pavucontrol flameshot -y
+update-alternatives --config x-terminal-emulator
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ~/.config/polybar-themes && chmod +x ~/.config/polybar-themes/setup.sh && ~/.config/polybar-themes/setup.sh
+git clone https://github.com/kevin010717/workspace.git ~/.workspace
