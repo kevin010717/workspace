b2a7c127cb4edd083c100b438721ddc157fc2887
kevin010717 <k511153362@gmail.com>
2024-11-03 10:59:39 +0800

    1

--- novnc.sh
+++ novnc.sh
@@ -2,5 +2,6 @@
 
 #noVNC
 sudo git clone https://github.com/novnc/noVNC.git $PREFIX/bin/noVNC
-iptables -A INPUT -p tcp --dport 6080 -j ACCEPT
+iptables -A INPUT -p tcp --dport 6080 -j ACCEPT # for termux
+sudo ufw allow 6080/tcp # for linnux
 $PREFIX/bin/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 6080
